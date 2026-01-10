b38070fb18e5954154782ef254009683e9089898
Kudzaishe George Zharare <kgzharare@gmail.com>
2026-01-10 14:30:00 +0200

    fix: update MemoryMathAddMinusMultDivGame to override generateCardPairs

--- src/games/MemoryMathAddMinusMultDivGame.js
+++ src/games/MemoryMathAddMinusMultDivGame.js
@@ -12,7 +12,7 @@ export class MemoryMathAddMinusMultDivGame extends MemoryGame {
         });
     }
 
-    setupLevel() {
+    generateCardPairs() {
         const numPairs = Math.min(3 + this.level, 8);
         const operations = [];
         const operationTypes = ['+', '-', '×', '÷'];
@@ -67,10 +67,7 @@ export class MemoryMathAddMinusMultDivGame extends MemoryGame {
             });
         });
         
-        this.calculateGridDimensions(this.cardPairs.length);
-        const { width, height } = this.cameras.main;
-        this.calculateOptimalLayout(width, height);
-        this.shuffleAndPositionCards();
+        this.totalPairs = numPairs;
     }
 
     createCard(x, y, cardData, index) {
