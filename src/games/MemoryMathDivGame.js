23d9c9947a7efcd7b0aa0a90bed2404a69fbf79c
Kudzaishe George Zharare <kgzharare@gmail.com>
2026-01-10 09:15:00 +0200

    fix: refactor MemoryMathDivGame and MemoryMathMultDivGame lifecycle methods

--- src/games/MemoryMathDivGame.js
+++ src/games/MemoryMathDivGame.js
@@ -12,7 +12,7 @@ export class MemoryMathDivGame extends MemoryGame {
         });
     }
 
-    setupLevel() {
+    generateCardPairs() {
         const numPairs = Math.min(3 + this.level, 8);
         const operations = [];
         
@@ -42,10 +42,7 @@ export class MemoryMathDivGame extends MemoryGame {
             });
         });
         
-        this.calculateGridDimensions(this.cardPairs.length);
-        const { width, height } = this.cameras.main;
-        this.calculateOptimalLayout(width, height);
-        this.shuffleAndPositionCards();
+        this.totalPairs = numPairs;
     }
 
     createCard(x, y, cardData, index) {
